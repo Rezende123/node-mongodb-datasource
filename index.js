c09649b4ada8e5bd1c79d18ba6e537f02362b0d6
Felipe Rezende <feliperezende.srz@gmail.com>
2020-02-19 08:16:50 -0300

    Iniciando projeto node

---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+module.exports = class Datasource {
+
+    constructor() {
+        this.MongoClient = require('mongodb').MongoClient;
+        this.database = null;
+        this.connectToDatabase();
+    }
+
+    set connectionUrl(url) {
+        this.urlConnection = url;
+    }
+
+    get connectionUrl() {
+        return this.urlConnection;
+    } 
+
+    getCollection(dbName, collectionName) {
+        return this.database.db(dbName).collection(collectionName);
+    }
+
+    connectToDatabase (uri = this.connectionUrl) {
+        if (this.database) {
+            return Promise.resolve(this.database);
+        }
+
+        return this.MongoClient.connect(uri, { useUnifiedTopology: true })
+            .then(db => {
+                this.database = db;
+                return this.database;
+            })
+            .catch(err => Error(err));
+    }
+
+    queryDatabase (dbName, collectionName, where = {}, options = {}) {
+        if (this.database) {
+            return this.getCollection(dbName, collectionName).find(where, options).toArray();
+        } else {
+            return Error("Can't connect to database.")
+        }
+    }
+
+    find(dbName, collectionName, where = {}, options = {}) {
+        return this.queryDatabase(dbName, collectionName, where, options);
+    }
+}
